4837aef8d3730cc22cb52d48454c4f4dbd9650ff
malu2335 <45488790+malu2335@users.noreply.github.com>
2021-02-20 08:37:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 
 
